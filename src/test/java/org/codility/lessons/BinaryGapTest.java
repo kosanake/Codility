b1c3471733e42d6c057c7586fb0edb25078a101d
home <asdanilov@gmail.com>
2020-03-05 22:28:57 +0300

    test

--- src/test/java/org/codility/lessons/BinaryGapTest.java
+++ src/test/java/org/codility/lessons/BinaryGapTest.java
@@ -9,10 +9,10 @@ public class BinaryGapTest {
     @Test
     public void shoudSolve() {
         BinaryGap binaryGap = new BinaryGap();
-        assertEquals(0, binaryGap.solve(0));
-        assertEquals(1, binaryGap.solve(10));
-        assertEquals(3, binaryGap.solve(10000));
-        assertEquals(4, binaryGap.solve(103874));
-        assertEquals(9, binaryGap.solve(2000000001));
+        assertEquals(0, binaryGap.solution(0));
+        assertEquals(1, binaryGap.solution(10));
+        assertEquals(3, binaryGap.solution(10000));
+        assertEquals(4, binaryGap.solution(103874));
+        assertEquals(9, binaryGap.solution(2000000001));
     }
 }
